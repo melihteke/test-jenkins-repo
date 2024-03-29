d24cd8c60b5410f5253449252e09be8eaea906c6
Melih Teke <36086368+melihteke@users.noreply.github.com>
2024-03-29 09:48:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ if len(sys.argv) > 1:
     name_parameter = sys.argv[1]
     print(f"Hello {name_parameter}")
 else:
+    print(len(sys.argv))
+    print(type(sys.argv))
     name_parameter = "Default Name"
-
-print(f"Hello {name_parameter}")
+    print(f"Hello {name_parameter}")

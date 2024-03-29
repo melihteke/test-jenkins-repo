a9b391567c3d898cd3cc026c89972884d0ab98f5
Melih Teke <36086368+melihteke@users.noreply.github.com>
2024-03-29 09:54:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
 
-if len(sys.argv) >= 1:
+if len(sys.argv) > 1:
     name_parameter = sys.argv[1]
-    print(len(sys.argv))
-    print(type(sys.argv))
+    print(len(sys.argv[1]))
+    print(type(sys.argv[1]))
     print(f"Hello {name_parameter}")
 else:
     print(len(sys.argv))
